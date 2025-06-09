e842f1ae6b07a8eb8ccb97fc33b2278fd2ccff3c
Dante-man-dotcom <kmukeshkoushik@gmail.com>
2025-06-09 14:23:43 +0530

    Delete SignIn directory

--- SignIn/demo_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser  # your custom user model
-
-@admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    pass
